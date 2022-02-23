7f3ee280d8783cf2907c30b83e708c005ab24797
obeliksgall <mosiu24@gmail.com>
2022-02-23 18:30:17 +0100

    0.0.1

---
+++ AfterINSTALLnoGUI.ps1
@@ -0,0 +1,64 @@
+﻿param([switch]$Elevated)
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ( (Test-Admin) -eq $false ) {
+    if ( $elevated ) {
+        Write-Host "Please run script as administrator"
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+'Script running with full privileges'
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device" -Name "DevicePasswordLessBuildVersion" -Value 0
+'Auto log in enable. Go to netplwiz'
+Set-ItemProperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin" -Value 0 #OR 5
+'Change UAC settings to 0'
+cmd.exe /c 'powercfg.exe /hibernate off'
+'Hibernate off'
+
+'Checking Winget...'
+if ( Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe ) {
+    'Winget Already Installed' 
+} else {
+    'Winget not found, installing it now'
+    $ResultText.text = "`r`n" +"`r`n" + "Installing Winget... Please Wait"
+	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+	$nid = (Get-Process AppInstaller).Id
+	Wait-Process -Id $nid
+	Write-Host Winget Installed
+    $ResultText.text = "`r`n" +"`r`n" + "Winget Installed - Ready for Next Task"
+}
+
+'Checking Chocolatey...'
+if ( Test-Path C:\ProgramData\chocolatey\choco.exe ) {
+    'Chocolatey Already Installed'
+} else {
+    'Chocolatey not found, installing it now'
+    $ResultText.text = "`r`n" +"`r`n" + "Installing Chocolatey... Please Wait"
+    $URLchocolatey = https://community.chocolatey.org/install.ps1
+    $ExecutionPolicy = Get-ExecutionPolicy
+    Set-ExecutionPolicy Unrestricted
+    New-Item -Path 'C:\' -Name 'Chocolatey_InstallScript.ps1' -ItemType File -Value $URLchocolatey.Content -Force
+	& 'C:\Chocolatey_InstallScript.ps1'
+	Write-Host Winget Installed
+    $ResultText.text = "`r`n" +"`r`n" + "Winget Installed - Ready for Next Task"
+    Set-ExecutionPolicy $ExecutionPolicy
+}
+
+
+
+
+
+
+
+
+
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
