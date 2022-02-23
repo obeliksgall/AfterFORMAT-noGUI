14f8ff0bbb7819e23726d317fb3c99e0cb6640cc
obeliksgall <mosiu24@gmail.com>
2022-02-23 18:48:01 +0100

    0.0.7

--- AfterINSTALLnoGUI.ps1
+++ AfterINSTALLnoGUI.ps1
@@ -13,17 +13,17 @@ if ( (Test-Admin) -eq $false ) {
     exit
 }
 
-'version 0.0.6`nScript running with full privileges'
+'version 0.0.7`n Script running with full privileges'
 
 $ExecutionPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy Unrestricted
 
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device" -Name "DevicePasswordLessBuildVersion" -Value 0
-'Auto log in enable. Go to netplwiz`n'
+'Auto log in enable. Go to netplwiz `n'
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name ConsentPromptBehaviorAdmin -Value 0 #OR 5
-'Change UAC settings to 0`n'
+'Change UAC settings to 0 `n'
 cmd.exe /c 'powercfg.exe /hibernate off'
-'Hibernate off`n'
+'Hibernate off `n'
 
 'Checking Winget...'
 if ( Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe ) {
@@ -33,7 +33,7 @@ if ( Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe ) {
 	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
 	$nid = (Get-Process AppInstaller).Id
 	Wait-Process -Id $nid
-	'Winget Installed`n'
+	'Winget Installed `n'
 }
 
 'Checking Chocolatey...'
@@ -42,9 +42,10 @@ if ( Test-Path C:\ProgramData\chocolatey\choco.exe ) {
 } else {
     'Chocolatey not found, installing it now'
     $URLchocolatey = "https://community.chocolatey.org/install.ps1"
-    New-Item -Path 'C:\' -Name 'Chocolatey_InstallScript.ps1' -ItemType File -Value $URLchocolatey.Content -Force
+    $chocolatey = Invoke-WebRequest -Uri $urlChocolatey -UserAgent 'Trident' -UseBasicParsing
+    New-Item -Path 'C:\' -Name 'Chocolatey_InstallScript.ps1' -ItemType File -Value $chocolatey.Content -Force
 	& 'C:\Chocolatey_InstallScript.ps1'
-	'Chocolatey Installed`n'
+	'Chocolatey Installed `n'
 }
 
 
